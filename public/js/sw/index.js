c9ecbd29c8286273bd1c13e54b3ed5f88b9452a3
aniansson <ebba@aniansson.se>
2018-01-02 13:55:27 +0100

    Change root url to skeleton url

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,12 +1,14 @@
-var staticCacheName = 'wittr-static-v3';
+var staticCacheName = 'wittr-static-v4';
 
 self.addEventListener('install', function(event) {
-  // TODO: cache /skeleton rather than the root page
 
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        // 3.26
+        // TODO: cache /skeleton rather than the root page
+        // changed '/' to '/skeleton'
+        '/skeleton',
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -33,8 +35,17 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  // 3.26
   // TODO: respond to requests for the root page with
   // the page skeleton from the cache
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
 
   event.respondWith(
     caches.match(event.request).then(function(response) {
